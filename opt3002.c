e0eb789482e9ba2d33d6949534012a88f07052e0
David Frey <dfrey@sierrawireless.com>
2019-10-15 09:54:09 -0700

    solution: metadata

--- opt3002.c
+++ opt3002.c
@@ -14,15 +14,6 @@ void __exit opt3002_exit(void)
 }
 module_exit(opt3002_exit);
 
-/*
- * The header file linux/module.h provides MODULE_LICENSE, MODULE_AUTHOR and
- * MODULE_DESCRIPTION macros to allow the author to specify metadata about the
- * module. Each of these macros expects a single string literal argument.
- *
- * == TASK ==
- * Define metadata for describing this module using each of the above macros.
- *
- * == TASK ==
- * On your mangOH, run "modinfo /legato/systems/current/modules/opt3002.ko" and
- * observe that the metadata is displayed.
- */
+MODULE_LICENSE("GPL v2");
+MODULE_AUTHOR("David Frey <dfrey@sierrawireless.com>");
+MODULE_DESCRIPTION("TI opt3002 light sensor driver");
