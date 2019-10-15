2542f69146bb18a8a7fe092646c7375e89860f38
David Frey <dfrey@sierrawireless.com>
2019-10-15 09:53:38 -0700

    task: metadata

--- opt3002.c
+++ opt3002.c
@@ -13,3 +13,16 @@ void __exit opt3002_exit(void)
 	printk("Goodbye opt3002!\n");
 }
 module_exit(opt3002_exit);
+
+/*
+ * The header file linux/module.h provides MODULE_LICENSE, MODULE_AUTHOR and
+ * MODULE_DESCRIPTION macros to allow the author to specify metadata about the
+ * module. Each of these macros expects a single string literal argument.
+ *
+ * == TASK ==
+ * Define metadata for describing this module using each of the above macros.
+ *
+ * == TASK ==
+ * On your mangOH, run "modinfo /legato/systems/current/modules/opt3002.ko" and
+ * observe that the metadata is displayed.
+ */
